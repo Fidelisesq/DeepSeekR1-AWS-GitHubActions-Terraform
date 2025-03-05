7737daee426b3de38fad855c0bbb1231d7137575
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-03-05 14:36:21 +0000

    push

--- main.tf
+++ main.tf
@@ -147,12 +147,18 @@ data "aws_key_pair" "existing_key" {
   key_pair_id = var.key_id
 }
 
+data "aws_subnet" "subnet_azs" {
+  count = lenght(var.private_subnet_ids)
+  id = var.private_subnet_ids[count.index]
+}
+
+
 # EC2 Instance (Private Subnet)
 resource "aws_instance" "deepseek_ec2" {
   ami             = var.ami_id
   instance_type   = var.instance_type
   key_name        = data.aws_key_pair.existing_key.key_name
-  subnet_id       = var.private_subnet_ids[0]
+  subnet_id       = data.aws_subnet.subnet_azs[0].id
   security_groups = [aws_security_group.deepseek_ec2_sg.id]
 
   root_block_device {
